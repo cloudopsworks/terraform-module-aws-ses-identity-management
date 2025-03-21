ed1507636cc4791df4086a01627438cbc1ec4e4a
Cristian Beraha <cristian@cloudops.works>
2025-03-21 12:59:00 -0300

    Refactor SES outputs to provide detailed attributes.
    This update restructures the SES outputs to include comprehensive details for both domains and emails. The new format uses a for loop to output nested attributes like DKIM status and configuration set names, improving informational clarity and utility. Sensitive flags are removed to accommodate the updated structure.

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,29 @@
 #            Distributed Under Apache v2.0 License
 #
 output "domains" {
-  value = aws_sesv2_email_identity.domain
-  sensitive = true
+  value = [
+    for item in aws_sesv2_email_identity.domain : {
+      arn               = item.arn
+      domain            = item.email_identity
+      type              = item.identity_type
+      configuration_set = item.configuration_set_name
+      dkim = {
+        status                    = item.dkim_attributes[0].status
+        signing_attributes_origin = item.dkim_attributes[0].signing_attributes_origin
+      }
+      verified_for_sending_status = item.verified_for_sending_status
+    }
+  ]
 }
 
 output "emails" {
-  value = aws_sesv2_email_identity.email
-  sensitive = true
+  value = [
+    for item in aws_sesv2_email_identity.email : {
+      arn                         = item.arn
+      email                       = item.email_identity
+      type                        = item.identity_type
+      configuration_set           = item.configuration_set_name
+      verified_for_sending_status = item.verified_for_sending_status
+    }
+  ]
 }
