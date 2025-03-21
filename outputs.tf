1c9fb073fa949a1bffe630ad079993ad2534708c
Cristian Beraha <cristian@cloudops.works>
2025-03-21 12:44:31 -0300

    Add outputs for SES email and domain identities
    Introduces `outputs.tf` to expose SES email and domain identities. This enables easier referencing of these resources in other modules or configurations.

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+##
+# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
+#            On GitHub: https://github.com/cloudopsworks
+#            Distributed Under Apache v2.0 License
+#
+output "domains" {
+  value = aws_sesv2_email_identity.domain
+}
+
+output "emails" {
+  value = aws_sesv2_email_identity.email
+}
