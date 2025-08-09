bfeae455120381354480c89c1af766a75925d068
Cristian Beraha <cristian@cloudops.works>
2025-08-09 17:27:10 -0300

    Enhance documentation and module configuration
    - Add comprehensive usage examples and detailed input/output variable descriptions in `README.md`.
    - Update `variables-ses.tf` to clarify SES identity configuration syntax.
    - Include AWS provider version requirement (`~> 5.89`) in `versions.tf`.
    - Modify `locals.tf` to use `data.aws_region.current.id` for region parsing.

--- variables-ses.tf
+++ variables-ses.tf
@@ -10,15 +10,16 @@
 ## SES Identity configuration
 # This variable is used to configure SES identities - below YAML format is expected
 # identities:
-#   domain: "example.com"
-#   email: "mail@example.com"
-#   dkim: true | false # (default: true)
-#   verification: true | false # (default: false)
-#   configuration_set: "<configuration set name>" # Optional
-#   dkim_private_key: "< provided private key >" # Optional
-#   dkim_selector: "< provided selector >" # Optional
-#   dkim_easy: true | false # (default: true)
-#   dkim_easy_key_length: "RSA_1024_BIT" | "RSA_2048_BIT" (default: "RSA_1024_BIT")
+#   <identity name>:
+#     domain: "example.com"
+#     email: "mail@example.com"
+#     dkim: true | false # (default: true)
+#     verification: true | false # (default: false)
+#     configuration_set: "<configuration set name>" # Optional
+#     dkim_private_key: "< provided private key >" # Optional
+#     dkim_selector: "< provided selector >" # Optional
+#     dkim_easy: true | false # (default: true)
+#     dkim_easy_key_length: "RSA_1024_BIT" | "RSA_2048_BIT" (default: "RSA_1024_BIT")
 variable "identities" {
   description = "SES Identity configuration"
   type        = any
